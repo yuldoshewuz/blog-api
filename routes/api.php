cf460e5e1fa1053f9b7c8349308e12d53afa82be
yuldoshewuz <iamfozilbek@gmail.com>
2026-01-25 22:16:19 +0500

    Added name to reset-password route.

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::post('/forgot-password', [PasswordResetController::class, 'sendResetLink']);
-Route::post('/reset-password', [PasswordResetController::class, 'reset']);
+Route::post('/reset-password', [PasswordResetController::class, 'reset'])->name('password.reset');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
